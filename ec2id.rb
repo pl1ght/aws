a4cae60986165e833392864859b0e127ceeca83e
pl1ght <forsaken@4saken.org>
2015-11-21 22:02:32 -0600

    added profile list and choices via user input.

--- ec2id.rb
+++ ec2id.rb
@@ -2,10 +2,27 @@ require 'aws-sdk'
 require 'json'
 
 
-# Grab AWS access/secret keys/region by profile from .aws/credentials file
+# Grab AWS access/secret keys by profile from .aws/credentials file
 
-credentials = Aws::SharedCredentials.new(profile_name: 'default')
-client = Aws::EC2::Client.new(credentials: credentials)
+# Windows
+#credentialfile = 'C:/Users/user/.aws/credentials'
+
+# Linux
+#credentialfile = '~user\.aws\credentials'
+
+list_creds = File.open credentialfile do |file|
+  file.find_all { |line| line =~ /\[\w+\]/ }
+end
+
+puts list_creds
+puts "Select profile: "
+profile = gets.chomp
+
+puts "Specify Region: "
+region = gets.chomp
+
+credentials = Aws::SharedCredentials.new(profile_name: profile )
+client = Aws::EC2::Client.new(credentials: credentials, region: region )
 
 resp = client.describe_instances
 
