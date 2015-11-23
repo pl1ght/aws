9b6afd0a9a8b541d0c62add41b504af33044c200
pl1ght <forsaken@4saken.org>
2015-11-23 14:13:00 -0600

    added basic export to csv file support

--- iamcreate.rb
+++ iamcreate.rb
@@ -1,13 +1,13 @@
 require 'aws-sdk'
 require 'securerandom'
-
+require 'csv'
 credentials = Aws::SharedCredentials.new(profile_name: 'changeme')
 iam = Aws::IAM::Resource.new(credentials: credentials, region: 'us-east-1')
 
 puts "Enter Number of IAM users to create: "
 maxcount = gets.chomp.to_i
 count = 0
-
+csvout = []
 
 while count < maxcount do
   num = rand(10000)
@@ -15,11 +15,16 @@ while count < maxcount do
   password = SecureRandom.base64(5)
   iamuser = iam.create_user(user_name: "#{user}")
   iamuser.create_login_profile(password: "#{password}", password_reset_required:false)
+  csvout << user + ", " + password #+ "\n"
 
-  print user + " "
-  print password + "\n"
+  CSV.open("iamcreds.csv", "w") do |csv|
+    csvout.each do |x|
+    csv << [x]
+      end
+  end
   count = count + 1
 end
 
-puts  "Finished"
+
+puts  "\nFinished"
 
