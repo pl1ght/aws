c2663c6f61e0764c3de359d6d8b1425383bda3c2
pl1ght <forsaken@4saken.org>
2015-11-24 08:22:40 -0600

    added modified elbec2id.rb script that reports back instance_ids/ip's/health of all instances behind specified ELB.

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # aws
 aws-sdk ruby v2 
+- Scripts that are just a part of various larger projects and one offs made to work standalone for future reference.
+
 
 s3auth.rb - just does an authenticate and s3 bucket list per user input region, then list contents of bucket specified
 
 ec2id.rb - allows user to auth via credentials file profiles and lists via Name tag/instance_id/powerstate. 
 
 iamcreate.rb - quick script that allows user to input how many IAM users requested. Then, the script will randomly generate a userXXXXX with a random base64 password, and create them on the AWS account profile specified. Had to do this for a lab account we were setting up for an AWS immersion day. Quick and easy.
+
+elbec2id.rb - pulls instance_id's, each instance_id's IP address, and the instance health for all instances registered with the specified ELB.
---
+++ elbec2id.rb
@@ -0,0 +1,33 @@
+require 'json'
+require 'aws-sdk'
+
+
+# Initialize Arrays
+ec2_ids=[]
+
+# Credentials pulled via .aws/credentials file via profiles
+credentials = Aws::SharedCredentials.new(profile_name: 'ro')
+
+# Create ELB client
+elb = Aws::ElasticLoadBalancing::Client.new(credentials: credentials, region: 'us-east-1')
+elbname = "QA-Gadfly-mgr"
+
+# Describe ELB specified
+elbid = elb.describe_load_balancers(options = {:load_balancer_names => ["#{elbname}"]})
+
+
+# Grab every instance behind an ELB
+elbid[:load_balancer_descriptions].first[:instances].each do |instance|
+  ec2_ids << instance.instance_id
+end
+
+# Create EC2 Client
+ec2 = Aws::EC2::Client.new(credentials: credentials, region: 'us-east-1')
+
+# Grab private IP address from each instance retrieved from ELB in previous block and populate ID/IP for each
+ec2_ids.each do |i|
+  aws_id = ec2.describe_instances(options = {:instance_ids => ["#{i}"]})
+  aws_id_resp = aws_id[:reservations].first[:instances].first[:private_ip_address]
+  aws_elb_resp = elb.describe_instance_health({load_balancer_name: "#{elbname}", instances:[{ instance_id: "#{i}"}]})
+  puts "#{ec2_ids} - #{aws_id_resp} - ELB Health = " + aws_elb_resp.instance_states[0].state
+end
