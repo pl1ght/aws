ff63d428a82ae326a99470319746df4b99169c2d
pl1ght <forsaken@4saken.org>
2015-12-11 14:34:09 -0600

    added dynamo client that just simply gets an item_count for a table.  needed this to find number of submitted jobs at any given time for a project with our chatops bot.

--- profile.rb
+++ profile.rb
@@ -1,12 +1,12 @@
 def awsprofile
   # Grab AWS access/secret keys by profile from .aws/credentials file
   # Windows
-  @winuser = ENV['USERPROFILE']
-  @credentialfile = "#{@winuser}/.aws/credentials"
+#  @winuser = ENV['USERPROFILE']
+#  @credentialfile = "#{@winuser}/.aws/credentials"
 
 # Linux/OSX
-#@nixuser = ENV['HOME']
-#@credentialfile = "#{@nixuser}/.aws/credentials"
+@nixuser = ENV['HOME']
+@credentialfile = "#{@nixuser}/.aws/credentials"
 
   list_creds = File.open @credentialfile do |file|
     file.find_all { |line| line =~ /\[\w+\]/ }
